349684cb8193f6e76b7ec4f9d05f8603c7c5b2d1
Oussamabennabi <oussamabennabi0184@gmail.com>
2024-06-18 16:52:33 +0100

    added: boilerplate

---
+++ src/app/(dashboard)/chats/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ComingSoon from "~/components/coming-soon";
+
+// users can chat with the owner
+const Page = () => {
+  return <ComingSoon />;
+};
+
+export default Page;
